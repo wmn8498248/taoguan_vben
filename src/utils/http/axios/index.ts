d98bb5e9fdecb0a72a2ec7030126abc68d58c7c4
DESKTOP-18AAJR1\laoma <280495605@qq.com>
2022-03-01 17:15:26 +0800

    修改token  和配置

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -41,6 +41,8 @@ const transform: AxiosTransform = {
     if (!isTransformResponse) {
       return res.data;
     }
+    
+
     // 错误的时候返回
 
     const { data } = res;
@@ -49,12 +51,13 @@ const transform: AxiosTransform = {
       throw new Error(t('sys.api.apiRequestFailed'));
     }
     //  这里 code，result，message为 后台统一的字段，需要在 types.ts内修改为项目自己的接口返回格式
-    const { code, result, message } = data;
+
+    const { code, msg } = data;
 
     // 这里逻辑可以根据项目进行修改
     const hasSuccess = data && Reflect.has(data, 'code') && code === ResultEnum.SUCCESS;
     if (hasSuccess) {
-      return result;
+      return data;
     }
 
     // 在此处根据自己项目的实际情况对不同的code执行不同的操作
@@ -68,8 +71,8 @@ const transform: AxiosTransform = {
         userStore.logout(true);
         break;
       default:
-        if (message) {
-          timeoutMsg = message;
+        if (msg) {
+          timeoutMsg = msg;
         }
     }
 
@@ -141,7 +144,7 @@ const transform: AxiosTransform = {
     const token = getToken();
     if (token && (config as Recordable)?.requestOptions?.withToken !== false) {
       // jwt token
-      (config as Recordable).headers.Authorization = options.authenticationScheme
+      (config as Recordable).headers.token = options.authenticationScheme
         ? `${options.authenticationScheme} ${token}`
         : token;
     }
